31a89f863146611a413a957360a5c9b1d48adb55
peterek01 <135986425+peterek01@users.noreply.github.com>
2024-10-17 16:30:03 +0200

    I made previous commits as comments in codio

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,6 +1,45 @@
+import json
+
+with open("animals_template.html", "r") as file:
+    content = file.read()
+    print(content)
 
 
-def main():
-    print("Hello world!")
+def load_data(file_path):
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
 
+animals = load_data('animals_data.json')
+
+
+def serialize_animal(animal_obj):
+    name = animal_obj["name"]
+    diet = animal_obj["characteristics"]["diet"]
+    location = animal_obj["locations"][0]
+    type_info = animal_obj["characteristics"].get("type", "Unknown")
+
+    return f'''
+        <li class="cards__item">
+            <div class="card__title">{name}</div>
+            <p class="card__text">
+                <ul>
+                    <li><strong>Diet:</strong> {diet}</li>
+                    <li><strong>Location:</strong> {location}</li>
+                    <li><strong>Type:</strong> {type_info}</li>
+                </ul>
+            </p>
+        </li>
+    '''
+
+
+animals_info = ""
+for animal_obj in animals:
+    animals_info += serialize_animal(animal_obj)
+
+final_html = content.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+
+with open("animals.html", "w") as output_file:
+    output_file.write(final_html)
+
+print("HTML file generated successfully")
