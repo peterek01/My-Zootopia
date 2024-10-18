360c05745a12346fedc7d0df0a0207a9cfbbc952
peterek01 <135986425+peterek01@users.noreply.github.com>
2024-10-18 13:28:11 +0200

    correct files with animals web generator from API

--- animals_web_generator_api.py
+++ animals_web_generator_api.py
@@ -1,19 +1,31 @@
-import requests
 import json
+import requests
+
+api_key = 'wm7dAuEaYpR3+Yck/Pz1/Q==GbXw4cWRi3NwahkI'
 
 animal_name = input("Enter an animal: ")
 
-with open("animals_template.html", "r") as file:
+url = f'https://api.api-ninjas.com/v1/animals?name={animal_name}'
+
+headers = {
+    "X-Api-Key": api_key
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    animals = response.json()
+    if animals:
+        print("Data fetched successfully from API")
+    else:
+        print("No data found for the given animal.")
+else:
+    print(f"Failed to retrieve data from API. Status code: {response.status_code}")
+    exit()
+
+
+with open('animals_template.html', 'r') as file:
     content = file.read()
-    print(content)
-
-
-def load_data(file_path):
-    with open(file_path, "r") as handle:
-        return json.load(handle)
-
-
-animals = load_data('animals_data.json')
 
 
 def serialize_animal(animal_obj):
@@ -26,23 +38,23 @@ def serialize_animal(animal_obj):
         <li class="cards__item">
             <div class="card__title">{name}</div>
             <p class="card__text">
-                <ul>
+                 <ul>
                     <li><strong>Diet:</strong> {diet}</li>
                     <li><strong>Location:</strong> {location}</li>
                     <li><strong>Type:</strong> {type_info}</li>
                 </ul>
             </p>
-        </li>
+         </li>
     '''
 
 
-animals_info = ""
+animals_info = ''
 for animal_obj in animals:
     animals_info += serialize_animal(animal_obj)
 
 final_html = content.replace("__REPLACE_ANIMALS_INFO__", animals_info)
 
-with open("animals.html", "w") as output_file:
+with open("animals_web_api.html", "w") as output_file:
     output_file.write(final_html)
 
-print("HTML file generated successfully")
+print("HTML file generated successfully.")
